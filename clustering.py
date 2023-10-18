54ef755b7e648dcf8b3085168fe93ca46613a187
IphixLi <iphigeniebera2024@u.northwestern.edu>
2023-10-18 00:52:34 -0500

    best keyword lead

---
+++ clustering.py
@@ -0,0 +1,56 @@
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import pairwise_distances
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import json
+
+
+f = open('stage/best_keyword.json',encoding="utf-8", errors="ignore")
+data=json.load(f)
+
+# Extract the keys (text items) from the dictionary
+texts = list(data.keys())
+
+# Create a TF-IDF vectorizer
+vectorizer = TfidfVectorizer()
+
+# Vectorize the texts and convert to a dense matrix
+X = vectorizer.fit_transform(texts).toarray()
+
+# Calculate Jaccard similarity between all pairs of texts
+jaccard_similarities = pairwise_distances(X, metric="jaccard")
+
+# Apply hierarchical clustering based on Jaccard similarity
+clustering = AgglomerativeClustering(n_clusters=None, distance_threshold=0.1, linkage='average', affinity='precomputed').fit(jaccard_similarities)
+
+# Get the cluster labels
+cluster_labels = clustering.labels_
+
+# Group the texts based on the cluster labels
+grouped_texts = {}
+for label, text in zip(cluster_labels, texts):
+    if label in grouped_texts:
+        grouped_texts[label].append(text)
+    else:
+        grouped_texts[label] = [text]
+
+with open("stage/best_clusters.txt", "w") as file:
+    with open("proposed_awards/proposed_from_best.txt", "w") as d:
+        for label, group in grouped_texts.items():
+            file.write(f"Cluster {label}:\n")
+            cluster = []
+            for text in group:
+                cluster.append([text, data[text]])
+                file.write(f"    {text} (Count: {data[text]})\n")
+            file.write("\n")
+            max_count_item = max(cluster, key=lambda x: x[1])
+
+            # if something had many iterations or was mentioned multiple times, it is more likely that it's a W
+            if len(cluster) > 1 or max_count_item[1] > 40:
+                max_count_item.append('1')
+            else:
+                max_count_item.append('0')
+
+            # proposed award, instances, confidence
+            d.write(f'{max_count_item[0]},{str(max_count_item[1])},{max_count_item[2]}\n')
+
---
+++ extraction/best_script.py
@@ -0,0 +1,104 @@
+import json
+import re
+# import nltk
+import collections
+
+f = open("../gg2013.json",encoding="utf-8", errors="ignore")
+json_text=json.load(f)
+track={}
+
+d = open("../gg2013answers.json",encoding="utf-8", errors="ignore")
+award_text=json.load(d)
+# for award in award_text["award_data"].keys():
+#     print(award)
+
+def normalize(text):
+    pattern = r'^RT @\w+: '
+    cleaned_text = re.sub(pattern, '', text)
+    
+    #remove hashtag
+    hash_pattern = r'#\w+'
+    hash_text = re.sub(hash_pattern, '', cleaned_text)
+    
+    tag_pattern=r'@\w+'
+    tag_text = re.sub(tag_pattern, '', hash_text)
+
+    non_english_pattern = r'[^a-zA-Z0-9\s-]'
+    cleaned_text = re.sub(non_english_pattern, '', tag_text)
+    
+    return cleaned_text.strip().lower()
+
+    
+hint_words=["drama","television","movie","film","award"]
+
+# store words in quotes"
+
+for entry in json_text:
+    #print(entry['text'].encode('utf-8'))
+    #print(normalize(entry['text']).encode('utf-8'))
+
+    normalized_text=normalize(entry['text'])
+    stop_words=['and', ' to',' at', 'goes',' is', 'like', 'not', 'she', 'http', 
+            'it','this', 'my', 'i ']
+    
+    if "best" in normalized_text:
+        punctuation_pattern = r'[?!.:;|]'
+        stop_pattern='|'.join([re.escape(s) for s in stop_words])
+        
+        split_text = re.split(punctuation_pattern, normalized_text)
+        nominated_corpus=split_text[0].strip()
+        # print(nominated_corpus.encode('utf-8'))
+        matches = nominated_corpus.split('best')
+        if len(matches)>1:
+            stop_split=re.split(stop_pattern, matches[1])
+            val='best '+stop_split[0].strip()
+            # val='best'+matches[1]
+            val.strip('"').strip()
+            #print(val.encode('utf-8'))
+
+            split_text=val.split(" ")
+            if len(split_text)<4:
+                continue
+            if ('actress' in val or 'actor' in val) and 'performance' not in val:
+                 continue
+            
+            words_to_check=['picture', 'drama', 'film', 'movie','comedy','musical']
+            if not any(val.lower().strip().endswith(word) for word in words_to_check):
+                continue
+
+            dash_split=val.split("-")
+
+            if len(dash_split)>1:
+                if any(word in dash_split[1] for word in ['picture', 'drama', 'film', 'movie','comedy']):
+                        val=dash_split[0].strip()+' - '+dash_split[1].strip()
+                else:
+                     val=dash_split[0].strip()
+
+            for_split=val.split(" for")
+
+                     
+            if len(for_split)>1:
+                if  'television' in for_split[1]:
+                    val=for_split[0].strip()+' for '+for_split[1].strip()
+                else:
+                    val=for_split[0].strip()
+            tv_pattern = r'tv'
+            val=re.sub(tv_pattern, 'television', val)
+
+            if not any(word in val for word in ['picture','musical','video', 'drama', 'film', 'movie','comedy']):
+                continue
+
+            if val not in track:
+                track[val]=0
+            track[val]+=1
+            #print(val.encode('utf-8'))
+            
+sorted_track=sorted(track.items(), key=lambda kv: kv[0], reverse=True)
+filtered_data = {key: value for key, value in sorted_track if value > 2 and len(key.split(" "))>3}
+sorted_dict = collections.OrderedDict(filtered_data)
+
+with open('stage/best_keyword.json', 'w', encoding='utf-8') as f:
+    json.dump(sorted_dict, f, ensure_ascii=False, indent=4)
+        # if 'best' in nominated_corpus:
+        #     print(nominated_corpus.encode('utf-8'))   
+    
